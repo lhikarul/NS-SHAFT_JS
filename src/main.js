b8461433fc369980afd54f1a2d4c09da0362c572
lhiakrul <711hikaru@gmail.com>
2020-05-16 23:11:00 +0800

    add: 遊戲入口配置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
+import {DataStore} from './js/base/DateStore';
+import { Ladder } from './js/runtime/Ladder';
+import { Director } from './js/Director';
 export class Main {
     constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
 
+        this.dataStore = DataStore.getInstance();
+        this.director = Director.getInstance();
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.dataStore.ctx = this.ctx;
+        this.init();
+    }
+    init () {
+        this.dataStore
+                    .put('wh',window.innerHeight)
+                    .put('ladder',Ladder)
+
+        this.director.createLadders();
+        this.director.run();
     }
 }
