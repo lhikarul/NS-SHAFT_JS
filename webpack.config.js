e327812252fe9cb0fe94769f36750cc33ae8ef60
lhiakrul <711hikaru@gmail.com>
2020-05-16 20:05:00 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/game.js',
+    mode: 'development',
+    devServer: {
+        contentBase: './bundle'
+    },
+    output: {
+        fileName: "bundle.js",
+        path: path.resolve(__dirname,'bundle')
+    },
+    resolve: {
+        alias: {
+            image: path.resolve(__dirname,'src/image')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '/images'
+                    }
+                }
+            }
+        ]
+    }
+}
