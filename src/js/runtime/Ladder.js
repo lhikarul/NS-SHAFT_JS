56b916c930e676f6fa1f681c4b7e62480f4b9de8
lhiakrul <711hikaru@gmail.com>
2020-05-17 11:27:19 +0800

    add: 繪製 fade, hurt, jump ladder

--- src/js/runtime/Ladder.js
+++ src/js/runtime/Ladder.js
@@ -1,5 +1,6 @@
 import { Vector } from "../base/Vector";
 import { DataStore } from "../base/DataStore";
+import {TweenMax} from "gsap";
 
 export class Ladder {
     constructor(args) {
@@ -30,8 +31,32 @@ export class Ladder {
             ctx.translate( this.p.x-this.width/2,this.p.y-this.extraHeight);
             ctx.fillStyle = "#888";
             ctx.font="20px Ariel"
-            ctx.fillRect(0,0,this.width,this.height / 2);
             ctx.fillText(this.type, 0 ,30)
+
+            if (this.type === "normal" || this.type === "hurt") {
+                ctx.fillRect(0,0,this.width,this.height / 2);
+            }
+            if (this.type == 'hurt') {
+                let span = this.width/16;
+                ctx.beginPath();
+                    for (let i=0; i <= this.width/span; i+= 1) {
+                        ctx.lineTo(i*span, -(i%2)*15);
+                    }
+                    ctx.fillStyle = "#ddd";
+                    ctx.fill();
+            }
+            
+            if (this.type === "jump") {
+                ctx.fillStyle = "#53d337";
+                ctx.fillRect(0,0,this.width,5);
+                ctx.fillRect(0,this.height+this.extraHeight,this.width,5)
+            }
+
+            if (this.type === "fade") {
+                ctx.fillStyle = "#ffd428";
+                ctx.fillRect(0,0,this.width,this.height / 2);
+            }
+
        ctx.restore();
     }
     step (player) {
@@ -42,6 +67,8 @@ export class Ladder {
 
         if (this.type === "jump") {
             player.v.y = -15;
+            this.extraHeight = 10;
+            TweenMax.to(this,0.2,{extraHeight: 0})
         }
         if (this.type === "slideLeft") {
             player.p.x -= 3;
